53b534fb01f6ef1d2a8c9b8078b5b67ca94772ea
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-05 11:29:48 -0400

    M06 - Modulo inicializado - pendiente

---
+++ appointments/src/test/java/cl/duoc/appointments/AppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
