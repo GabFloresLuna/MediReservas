c7cf61cd97065ff4c119aed8d7e46b16ffbccc13
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-05 11:32:00 -0400

    M09 - Modulo inicializado - pendiente

---
+++ prescriptions/src/test/java/cl/duoc/prescriptions/PrescriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.prescriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
