17c41e28e4448791f05edc0dfcedf1ded7c84d48
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-05 11:37:19 -0400

    M10 - Modulo inicializado - pendiente

---
+++ notifications/src/test/java/cl/duoc/notifications/NotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
