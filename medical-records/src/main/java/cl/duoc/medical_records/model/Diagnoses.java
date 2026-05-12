584c56be912cbd012bfba91cdeda190fc8f99682
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-12 10:44:37 -0400

    M08 - Modelo Diagnoses creado - funciona

---
+++ medical-records/src/main/java/cl/duoc/medical_records/model/Diagnoses.java
@@ -0,0 +1,37 @@
+package cl.duoc.medical_records.model;
+ 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "diagnoses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Diagnoses 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diagnosis_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "medical_visit_id", nullable = false)
+    private MedicalVisit medicalVisit;
+
+    @Column(name = "diagnosis_description", nullable = false, length = 255)
+    private String diagnosisDescription;
+
+    @Column(name = "diagnosis_notes", columnDefinition = "TEXT")
+    private String diagnosisNotes;
+}
