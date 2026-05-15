4b83c2093a743e8b5abc65189f7320a31026ad77
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-15 01:08:51 -0400

    M08 - repositorios MedicalRecord, MedicalVisit actualizados - funciona

--- medical-records/src/main/java/cl/duoc/medical_records/repository/MedicalVisitRepository.java
+++ medical-records/src/main/java/cl/duoc/medical_records/repository/MedicalVisitRepository.java
@@ -2,10 +2,10 @@ package cl.duoc.medical_records.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 import cl.duoc.medical_records.model.MedicalVisit;
 
 @Repository
-public interface MedicalVisitRepository extends JpaRepository<MedicalVisit,Long>{
-
+public interface MedicalVisitRepository extends JpaRepository<MedicalVisit,Long>
+{ 
 }
