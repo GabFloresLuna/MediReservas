fbf7a6b9d30aa8be124f6f0a65289bed7c88fb6d
Antonio Cayulen <an.cayulen@duocuc.cl>
2026-05-16 23:54:31 +0000

    M09 - Modelo generado - Funciona

---
+++ prescriptions/src/main/java/cl/duoc/prescriptions/model/Prescription.java
@@ -0,0 +1,44 @@
+package cl.duoc.prescriptions.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "prescriptions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long prescriptionId;
+
+    @Column(name = "medical_visit_id", nullable = false)
+    private Long medicalVisitId;
+
+    @Column(name = "patient_user_id", nullable = false)
+    private Long patientUserId;
+
+    @Column(name = "doctor_id", nullable = false)
+    private Long doctorId;
+
+    @Column(name = "issued_at", nullable = false)
+    private LocalDateTime issuedAt;
+
+    @Column(name = "prescription_status", nullable = false, length = 30)
+    private String prescriptionStatus;
+
+    @Column(name = "notes", length = 255)
+    private String notes;
+}
