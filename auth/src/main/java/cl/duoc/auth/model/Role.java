fd374001e764000fb017e65c60fda2548db5f358
Hanrol <be.cubillosp@duocuc.cl>
2026-05-17 02:47:11 -0400

    M01 - Creacion de modelos - Funciona

---
+++ auth/src/main/java/cl/duoc/auth/model/Role.java
@@ -0,0 +1,43 @@
+package cl.duoc.auth.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "role_name", nullable = false, unique = true, length = 50)
+    private String roleName;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "active", nullable = false)
+    private boolean active = true;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<AuthUser> users = new HashSet<>();
+}
