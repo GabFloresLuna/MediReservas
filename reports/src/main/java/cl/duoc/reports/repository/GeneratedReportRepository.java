711665f0954545af1107e9f76c1fb0610bc91603
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-18 18:15:01 -0400

    M11 - repositorio actualizado - funciona

--- reports/src/main/java/cl/duoc/reports/repository/GeneratedReportRepository.java
+++ reports/src/main/java/cl/duoc/reports/repository/GeneratedReportRepository.java
@@ -3,9 +3,9 @@ package cl.duoc.reports.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import cl.duoc.reports.model.GeneratedReports;
+import cl.duoc.reports.model.GeneratedReport;
 
 @Repository
-public interface GeneratedReportsRepository extends JpaRepository<GeneratedReports, Long>{
+public interface GeneratedReportRepository extends JpaRepository<GeneratedReport, Long>{
 
 }
