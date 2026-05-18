7986fa7bf00d9c3c3748520f23efb4e3c7b26b5f
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-18 18:16:42 -0400

    M11 - service creado - funciona

---
+++ reports/src/main/java/cl/duoc/reports/service/GeneratedReportService.java
@@ -0,0 +1,38 @@
+package cl.duoc.reports.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.reports.dto.CreateGeneratedReportRequestDTO;
+import cl.duoc.reports.dto.GeneratedReportResponseDTO;
+import cl.duoc.reports.extras.ToDTO;
+import cl.duoc.reports.model.GeneratedReport;
+import cl.duoc.reports.repository.GeneratedReportRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GeneratedReportService 
+{
+    @Autowired
+    private ToDTO toDTO;
+    private final GeneratedReportRepository generatedReportsRepository;
+    
+    public GeneratedReportResponseDTO create(CreateGeneratedReportRequestDTO requestDTO)
+    {
+        GeneratedReport generatedReport = toDTO.toGeneratedReport(requestDTO);
+        generatedReportsRepository.save(generatedReport);
+        return toDTO.toGeneratedReportResponseDTO(generatedReport);
+    }
+
+    public List<GeneratedReportResponseDTO> listAll()
+    {
+        return generatedReportsRepository.findAll()
+            .stream()
+            .map(x -> toDTO.toGeneratedReportResponseDTO(x))
+            .collect(Collectors.toList());
+    }
+}
---
+++ reports/src/main/java/cl/duoc/reports/service/ReportRequestService.java
@@ -0,0 +1,39 @@
+package cl.duoc.reports.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
+import cl.duoc.reports.dto.CreateReportRequestDTO;
+import cl.duoc.reports.dto.ReportRequestResponseDTO;
+import cl.duoc.reports.extras.ToDTO;
+import cl.duoc.reports.model.ReportRequest;
+import cl.duoc.reports.repository.ReportRequestRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReportRequestService 
+{
+    @Autowired
+    private ToDTO toDTO;
+
+    private final ReportRequestRepository reportRequestRepository;
+
+    public ReportRequestResponseDTO create(CreateReportRequestDTO requestDTO)
+    {
+        ReportRequest reportRequest = toDTO.toReportRequest(requestDTO);
+        reportRequestRepository.save(reportRequest);
+        return toDTO.toReportRequestResponseDTO(reportRequest);
+    }
+
+    public List<ReportRequestResponseDTO> listAll()
+    {
+        return reportRequestRepository.findAll()
+            .stream()
+            .map(x -> toDTO.toReportRequestResponseDTO(x))
+            .collect(Collectors.toList());
+    }
+}
