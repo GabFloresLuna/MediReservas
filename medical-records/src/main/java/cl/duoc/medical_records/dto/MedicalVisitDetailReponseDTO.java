566a285ca645daeac1b0c08cc6d31c061c4ffc7d
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-19 10:43:50 -0400

    M08 - dtos formato de fecha corregido - funciona

--- medical-records/src/main/java/cl/duoc/medical_records/dto/MedicalVisitDetailReponseDTO.java
+++ medical-records/src/main/java/cl/duoc/medical_records/dto/MedicalVisitDetailReponseDTO.java
@@ -3,15 +3,19 @@ package cl.duoc.medical_records.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public record MedicalVisitDetailReponseDTO
 (
     Long medicalVisitId,
     Long appointmentId,
     Long doctorId,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime visitDate,
     String visitReason,
     String observations,
     String treatment,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createdAt,
     List<DiagnosisResponseDTO> diagnoses,
     List<VitalSignResponseDTO> vitalSigns
--- medical-records/src/main/java/cl/duoc/medical_records/dto/MedicalVisitResponseDTO.java
+++ medical-records/src/main/java/cl/duoc/medical_records/dto/MedicalVisitResponseDTO.java
@@ -2,15 +2,19 @@ package cl.duoc.medical_records.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public record MedicalVisitResponseDTO
 (
     Long medicalVisitId,
     Long medicalRecordId,
     Long appointmentId,
     Long doctorId,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime visitDate,
     String visitReason,
     String observations,
     String treatment,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createdAt
 ) {}
