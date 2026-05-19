ada00457c7f1cc734a7b733fb9d43d60cf1b73bd
GabFloresLuna <gab.floresl@duocuc.cl>
2026-05-19 10:40:05 -0400

    M11 - dto formato de fecha corregido - funciona

--- reports/src/main/java/cl/duoc/reports/dto/GeneratedReportResponseDTO.java
+++ reports/src/main/java/cl/duoc/reports/dto/GeneratedReportResponseDTO.java
@@ -2,12 +2,16 @@ package cl.duoc.reports.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+ 
+
 public record GeneratedReportResponseDTO
 (
     Long generatedReportId,
     Long reportRequestId,
     Long generatedByUserId,
     String reportType,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime generatedAt,
     String reportFormat,
     String filePath,
