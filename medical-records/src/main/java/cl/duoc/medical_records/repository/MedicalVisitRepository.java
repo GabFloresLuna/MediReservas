b724cb1342294868e436674f2b29e14f57c1f89b
GabFloresLuna <gab.floresl@duocuc.cl>
2026-06-02 12:04:32 -0400

    M08 - Repositorio actualizado - funciona

--- medical-records/src/main/java/cl/duoc/medical_records/repository/MedicalVisitRepository.java
+++ medical-records/src/main/java/cl/duoc/medical_records/repository/MedicalVisitRepository.java
@@ -1,6 +1,9 @@
 package cl.duoc.medical_records.repository;
  
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
  
@@ -9,4 +12,5 @@ import cl.duoc.medical_records.model.MedicalVisit;
 @Repository
 public interface MedicalVisitRepository extends JpaRepository<MedicalVisit,Long>
 { 
+    public List<Optional<MedicalVisit>> findAllByPatientId(Long patientId);
 }
