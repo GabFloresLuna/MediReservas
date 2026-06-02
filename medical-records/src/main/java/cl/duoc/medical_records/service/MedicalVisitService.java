719d7fbe1d71ba57d6a39a52ed7bb71005e66d88
GabFloresLuna <gab.floresl@duocuc.cl>
2026-06-02 12:02:49 -0400

    M08 - service actualizado - funciona

--- medical-records/src/main/java/cl/duoc/medical_records/service/MedicalVisitService.java
+++ medical-records/src/main/java/cl/duoc/medical_records/service/MedicalVisitService.java
@@ -1,5 +1,8 @@
 package cl.duoc.medical_records.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,13 +33,16 @@ public class MedicalVisitService
         return toDTO.toMedicalVisitResponseDTO(medicalVisit);
     }
 
-    public MedicalVisitDetailReponseDTO findById(Long id)
+    public List<MedicalVisitDetailReponseDTO> findAllById(Long id)
     {
         //Validación y asignación de visita médica
-        MedicalVisit medicalVisit = medicalVisitRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("No existe una visita médica registrada con ese ID"));
+        List<MedicalVisit> medicalVisit = medicalVisitRepository.findAllByPatientId(id)
+            .stream()
+            .map(x -> x.orElseThrow(() -> new RuntimeException("No existe una visita médica registrada con ese ID")))
+            .collect(Collectors.toList());
         
         //Creación y retorno de responseDTO
-        return toDTO.toMedicalVisitDetailReponseDTO(medicalVisit);
+        return medicalVisit.stream().map(x -> toDTO.toMedicalVisitDetailReponseDTO(x)).collect(Collectors.toList());
     }
+
 }
