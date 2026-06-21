2980b27cfe59dea9752a8d67aa024de8346f4574
CrisPSdev <crispardos@pm.me>
2026-06-20 21:48:29 -0400

    10 - add config -test

---
+++ notifications/src/main/java/cl/duoc/notifications/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package cl.duoc.notifications.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+// Cambio: bean WebClient.Builder @LoadBalanced para llamar a otros microservicios vía Eureka.
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
